a933db2b07bd1bda1e4d785e4651a24432d1ad84
Sujith K S <sujith.ks@sonata-software.com>
2023-04-18 00:52:08 +0530

    From scratch code buil

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -9,17 +9,16 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     return requirements
 
 
 setup(
-    name='RegressorProject',
+    name='DiamondPredictionProject',
     version='0.0.1',
-    author='Krissh',
-    author_email='krish.naik@ineuron.ai',
+    author='Sujith',
+    author_email='sujithks81@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
-)
+)            
